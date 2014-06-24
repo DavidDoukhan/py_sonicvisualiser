d4d6fe492853fb824b77120f919fbbba02d8d134
David Doukhan <david.doukhan@gmail.com>
2014-06-25 00:34:44 +0200

    change peppy module name in comments

--- py_sonicvisualiser/SVEnv.py
+++ py_sonicvisualiser/SVEnv.py
@@ -2,14 +2,14 @@
 #
 # Copyright (c) 2013 David Doukhan <david.doukhan@gmail.com>
 
-# This file is part of py-sonic-visualiser.
+# This file is part of py_sonicvisualiser.
 
-# py-sonic-visualiser is free software: you can redistribute it and/or modify
+# py_sonicvisualiser is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 
-# py-sonic-visualiser is distributed in the hope that it will be useful,
+# py_sonicvisualiser is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
