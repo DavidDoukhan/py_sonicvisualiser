bc017cfa6cccf5a6dc856af4b1faba3a46a09de6
David Doukhan <david.doukhan@gmail.com>
2014-06-26 14:54:10 +0200

    some bugs removed

--- examples/example1.py
+++ examples/example1.py
@@ -1,29 +1,28 @@
 import sys
+import numpy as np
 from py_sonicvisualiser import SVEnv
 
 wavfname = sys.argv[1]
-outsvenvname = sys.argv[2]
+outsvenvfname = sys.argv[2]
 
 # init a sonic visualiser environment file corresponding
 # to the analysis of media wavfname
 sve = SVEnv(wavfname)
 
-# add a spectrogram view
-sve.add_spectrogram()
+# append a spectrogram view
+specview = sve.add_spectrogram()
 
 # append a continuous annotation layer corresponding to a sinusoidal signal
+# on the spectrogram view previously defined
 x = np.array(range(10000, 20000, 5)) / 1000.
-ca = t.add_continuous_annotations(x, 1 + 3 * np.sin(2 * x))
+sve.add_continuous_annotations(x, 1 + 3 * np.sin(2 * x), view=specview)
 
-# add a spectrogram in the same view than the sinusoidal continuous annotation
-t.add_spectrogram(view=ca)
-
-# append a labelled interval annotation layer
+# append a labelled interval annotation layer on a new view
 intvtime = [1., 5., 21.5]
 intvdur = [3., 11., 5.]
 intvlabel = ['myintv1', 'mywonderfull  intv2', 'intv3']
 intvval = [0, 1, 5]
-t.add_interval_annotations(intvtime,intvdur,intvlabel,intvval)
+sve.add_interval_annotations(intvtime,intvdur,intvlabel,intvval)
 
 # save the environment to a sonic visualiser environment file
-t.save(outsvenvfname)
+sve.save(outsvenvfname)
